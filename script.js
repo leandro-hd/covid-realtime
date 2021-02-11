de468f0960efdba060dfead967b2edee29f95638
leandro-hd <leandrohg2003@gmail.com>
2021-02-11 19:28:10 -0300

    Atribuição de objetos ao HTML

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
-let xhr = new XMLHttpRequest();
+let country = new XMLHttpRequest();
+let confirmed = new XMLHttpRequest();
+let recovered = new XMLHttpRequest();
+let deaths = new XMLHttpRequest();
 
-xhr.open('GET', 'https://api.covid19api.com/countries');
+const countriesSelect = document.getElementById("country");
 
-xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-xhr.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-xhr.setRequestHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+country.open('GET', 'https://api.covid19api.com/countries');
+confirmed.open('GET', 'https://api.covid19api.com/dayone/country/south-africa/status/confirmed');
+recovered.open('GET', 'https://api.covid19api.com/dayone/country/south-africa/status/recovered');
+deaths.open('GET', 'https://api.covid19api.com/dayone/country/south-africa/status/deaths');
 
-xhr.setRequestHeader('Content-Type', 'application/json'); //Obrigatorio API
-// xhr.setRequestHeader('access_token', '5cf9dfd5-3449-485e-b5ae-70a60e997864'); //Obrigatorio API
-
-
-xhr.onreadystatechange = () => {
-
-  if(xhr.readyState == 4) {
-      if(xhr.status == 200) {
-          console.log(JSON.parse(xhr.responseText));
-      } 
+country.onreadystatechange = () => {
+  if(country.readyState == 4) {
+    if(country.status == 200) {
+      const countryName = JSON.parse(country.responseText);
+      countryName.sort(function (a, b) {	
+        return (a.Country > b.Country) ? 1 : ((b.Country > a.Country) ? -1 : 0);         
+      });
+      for (let Country in countryName) {
+        countryName.hasOwnProperty(Country);
+        countryList = countryName[Country].Country;
+        option = new Option(countryList);
+        countriesSelect.options[countriesSelect.options.length] = option;
+      }
+    } 
   }
-
 };
 
-xhr.send();
+country.send();
 
+confirmed.onreadystatechange = () => {
+
+  if(confirmed.readyState == 4) {
+    if(confirmed.status == 200) {
+      const json = JSON.parse(confirmed.responseText); 
+      console.log("Confirmados: " + json[246].Cases);
+    } 
+  }
+};
+
+confirmed.send();
+
+recovered.onreadystatechange = () => {
+
+  if(recovered.readyState == 4) {
+    if(recovered.status == 200) {
+      const json = JSON.parse(recovered.responseText); 
+      console.log("Recuperados: " + json[246].Cases);
+    } 
+  }
+};
+
+recovered.send();
+
+deaths.onreadystatechange = () => {
+
+  if(deaths.readyState == 4) {
+    if(deaths.status == 200) {
+      const json = JSON.parse(deaths.responseText); 
+      console.log("Mortos: " + json[246].Cases);
+    } 
+  }
+};
+
+deaths.send();
